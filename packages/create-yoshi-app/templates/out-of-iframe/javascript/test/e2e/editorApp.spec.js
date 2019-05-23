5a31069318e46fc5f2cbab951dbc87ed4b04e91a
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-05-23 11:49:17 +0300

    Clean generators, remove redundant waitForSelector

--- packages/create-yoshi-app/templates/out-of-iframe/javascript/test/e2e/editorApp.spec.js
+++ packages/create-yoshi-app/templates/out-of-iframe/javascript/test/e2e/editorApp.spec.js
@@ -1,7 +1,6 @@
 describe('React application', () => {
   it('should display title', async () => {
     await page.goto('https://localhost:3100/editorApp');
-    await page.waitForSelector('h2');
 
     expect(await page.$eval('h2', e => e.innerText)).toEqual('Hello World!');
   });
--- packages/create-yoshi-app/templates/out-of-iframe/typescript/test/e2e/editorExampleWidget.spec.ts
+++ packages/create-yoshi-app/templates/out-of-iframe/typescript/test/e2e/editorExampleWidget.spec.ts
@@ -1,7 +1,6 @@
 describe.skip('React application', () => {
   it('should display title', async () => {
     await page.goto('https://localhost:3100/editorExampleWidget');
-    await page.waitForSelector('h2');
 
     expect(await page.$eval('h2', e => e.innerText)).toEqual('Hello World!');
   });
