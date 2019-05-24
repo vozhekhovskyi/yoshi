2e796082b27dbe87de748ecbaff5b95d541e1a51
Volodymyr Ozhekhovskyi <volodymyro@wix.com>
2019-05-24 19:44:23 +0300

    fix

--- pr-surge-comment.js
+++ pr-surge-comment.js
@@ -4,17 +4,27 @@ const parseArgs = require('minimist');
 const promisify = require('promisify');
 const GitHub = require('github-api');
 const { readFileSync } = require('fs');
+const jwt = require('jsonwebtoken');
+
+const privateKey = readFileSync('test-pr-surge-comment.2019-05-24.private-key');
+const now = Math.floor(Date.now() / 1000);
+const payload = {
+  iat: now, // Issued at time
+  exp: now + 60, // JWT expiration time
+  iss: '31408',
+};
+const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
 
 const argv = parseArgs(process.argv.slice(2), {
   string: ['pr'],
 });
 
-const token = '49fcfde55772e23f5c7792bb81a0842d6fe7916c';
+// const token = '49fcfde55772e23f5c7792bb81a0842d6fe7916c';
 
-if (!token.length) {
-  console.warn('No token for GitHub, ignoring');
-  process.exit(0);
-}
+// if (!token.length) {
+//   console.warn('No token for GitHub, ignoring');
+//   process.exit(0);
+// }
 
 const gh = new GitHub({ token });
 const PRNumber = argv.pr;
@@ -26,7 +36,6 @@ const issues = promisify.object({
 
 (async () => {
   try {
-    
     await issues.createIssueComment(PRNumber, `[Surge build](wix-yoshi-${PRNumber}.surge.sh/yoshi)`);
   } catch (e) {
     console.error(e.message);
